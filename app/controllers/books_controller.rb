ab3d3f4f1563c69dea17119f8c8efd0d8ab2a19c
EC2 Default User <ec2-user@ip-172-31-31-150.ap-northeast-1.compute.internal>
2023-02-12 09:58:44 +0000

    [Add]view(users_index,users_show,books_index)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,12 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
+    @book = Book.new
   end
   
   def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,12 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
-    @books = Book.all
   end
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit

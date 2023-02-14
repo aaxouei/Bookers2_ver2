3e46a9cca10f21a44d297dc4294bd354eaa54543
EC2 Default User <ec2-user@ip-172-31-31-150.ap-northeast-1.compute.internal>
2023-02-14 01:21:29 +0000

    [Fix]authenticate_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :not_login_user_redirect
+  before_action :authenticate_user!,except: [:top,:about]
     def after_sign_in_path_for(resource)
       user_path(current_user)
     end
-    
+
     def after_sign_out_path_for(resource)
       root_path
     end
-    
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
-  
-  def not_login_user_redirect
-    unless user_signed_in?
-        redirect_to new_user_session_path
-    end
-  end
+
 end
